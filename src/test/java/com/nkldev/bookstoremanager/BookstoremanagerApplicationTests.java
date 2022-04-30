f49d11f0e2de093af8523a618114979e65b6d5b1
Felipe Augusto Cabral <felipeaugustocabral@gmail.com>
2022-04-30 16:29:02 -0300

    First commit: Project struture created

---
+++ src/test/java/com/nkldev/bookstoremanager/BookstoremanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nkldev.bookstoremanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoremanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
