ae3de9d9321ff5cc2f41005fea1dbff682b31a04
Felipe Augusto Cabral <felipeaugustocabral@gmail.com>
2022-04-30 17:48:22 -0300

    Added entities author and book, and data jpa  dependency on pom.xml

---
+++ src/main/java/entity/author.java
@@ -0,0 +1,20 @@
+package entity;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@entity
+public class author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Column(nullable = false)
+    private Integer age;
+}
